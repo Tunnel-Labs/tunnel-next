77d84850f12f13e724c9d2ef1ba2fb1c9b75bdc3
David Thomas <github@davthom.xyz>
2024-01-29 14:34:28 -0500

    feat: add Tunnel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { TunnelToolbar } from "@tunnel/nextjs";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -11,7 +12,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        {(process.env.NODE_ENV !== "developement" && process.env.RAILWAY_GIT_BRANCH !== 'main') && (
+          <TunnelToolbar 
+            projectId={process.env.TUNNEL_PROJECT_ID}
+            branch={process.env.RAILWAY_GIT_BRANCH}
+          />
+        )}
+      </body>
     </html>
   );
 }
